f34d4d18db979e83f327f24604608ec3b3dcc055
smigowsk99 <smigowski.marcin@gmail.com>
2022-10-16 09:33:01 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/lab1/builder/Person.java
@@ -0,0 +1,15 @@
+package pl.edu.wszib.lab1.builder;
+
+public class Person {
+    private final String name;
+    private final String surname;
+    private final String adress;
+    private final String gender;
+    private final String age;
+
+}
+
+
+
+
+}
